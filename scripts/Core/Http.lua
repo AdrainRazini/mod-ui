c335913916a3e1001d2e8408f8603d05a3f56b5f
Adrian Razini Rangel <160901383+AdrainRazini@users.noreply.github.com>
2026-02-03 23:57:06 +0000

    Comit 05

--- scripts/Core/Http.lua
+++ scripts/Core/Http.lua
@@ -1,6 +1,6 @@
 -- scripts/Core/Http.lua
 -- novo Objetivo Fazer ser hibrido para server ou scripters
-
+-- Obj: Conexão https + jogo 
 
 local HttpService = game:GetService("HttpService")
 
@@ -23,7 +23,7 @@ Http._initialized = false
 function Http:Init(resolverUrl)
 	resolverUrl = resolverUrl or "http://localhost:3000/resolver"
 
-	if Http._initialized then
+	if self._initialized then
 		return true
 	end
 
@@ -38,9 +38,10 @@ function Http:Init(resolverUrl)
 	end
 
 	local data = HttpService:JSONDecode(response)
-    Http.Domain = data.base and data.base.root
-	Http.BaseUrl = data.base and data.base.api
-	Http.Endpoints = data.endpoints or {}
+	
+	self.Domain = data.base and data.base.root
+	self.BaseUrl = data.base and data.base.api
+	self.Endpoints = data.endpoints or {}
 
 	if not Http.BaseUrl then
 		warn("[Mod_UI][HTTP] Resolver inválido")
@@ -48,7 +49,8 @@ function Http:Init(resolverUrl)
 	end
 
 	print("[Mod_UI][HTTP] Resolver carregado:", Http.BaseUrl)
-	Http._initialized = true
+	
+	self._initialized = true
 	return true
 end
 
@@ -83,7 +85,14 @@ end
 -- GET Module (Lua)
 -- ============================
 
+local RunService = game:GetService("RunService")
+
 function Http:GetModule(path)
+	if RunService:IsServer() then
+		warn("[Mod_UI][HTTP] GetModule não permitido no Server")
+		return nil
+	end
+
 	local code = self:GetRaw(path)
 	if not code then return nil end
 
@@ -102,30 +111,16 @@ function Http:GetModule(path)
 	return result
 end
 
-
 function Http:GetByName(name)
-	local endpoint = Http.Endpoints[name]
+	local endpoint = self.Endpoints[name]
 	if not endpoint then
 		warn("[Mod_UI][HTTP] Endpoint não encontrado:", name)
 		return nil
 	end
 
-	if Cache[endpoint] then
-		return Cache[endpoint]
-	end
-
-	local success, response = pcall(function()
-		return HttpService:GetAsync(endpoint)
-	end)
-
-	if not success then
-		warn("[Mod_UI][HTTP] Falha ao buscar endpoint:", name)
-		return nil
-	end
-
-	Cache[endpoint] = response
-	return response
+	return self:GetRaw(endpoint:gsub(self.BaseUrl .. "/", ""))
 end
+
 
 
 return Http
@@ -141,8 +136,7 @@ Http = {
 
   -- base resolvida automaticamente
   BaseUrl = "https://mod-ui.vercel.app/api",
-  Domain = "https://mod-ui.vercel.app/",
-
+  Domain = "https://mod-ui.vercel.app/"
   -- endpoints absolutos vindos do backend
   Endpoints = {
     health   = "https://mod-ui.vercel.app/health",
@@ -157,21 +151,5 @@ Http = {
   GetModule = function(self, path) end,
   GetByName = function(self, name) end
 }
-
-
-
-
--- prints
-{
-                    ["BaseUrl"] = "http://localhost:3000/api",
-                    ["Domain"] = "http://localhost:3000",
-                    ["Endpoints"] =  ▶ {...},
-                    ["GetByName"] = "function",
-                    ["GetModule"] = "function",
-                    ["GetRaw"] = "function",
-                    ["Init"] = "function",
-                    ["__index"] = "*** cycle table reference detected ***",
-                    ["_initialized"] = true
-}  -  Server - Script:17
 
 ]]
