5def44847497578509fc14c7b9d5bcead4e12e57
Adrian Razini Rangel <160901383+AdrainRazini@users.noreply.github.com>
2026-04-13 10:20:21 +0000

    Alpha

--- scripts/Mods/Folder_The_Tribe_Survival/Tribe_Survival.lua
+++ scripts/Mods/Folder_The_Tribe_Survival/Tribe_Survival.lua
@@ -254,7 +254,7 @@ local function ApplyAim()
 
 	if not targetRoot or not myRoot then return end
 
-	SmoothLockRoot(myRoot, targetRoot.Position, 0.2)
+	SmoothLockRoot(myRoot, targetRoot.Position, 0)
 end
 
 -- =========================
